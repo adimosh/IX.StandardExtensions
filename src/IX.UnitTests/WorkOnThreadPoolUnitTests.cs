bec2398985c951fb04d7de91a464c810c23a2ac2
Adrian Mos <adimosh@outlook.com>
2020-05-15 09:43:10 +0300

    Modified unit test to best serve threading.

--- src/IX.UnitTests/WorkOnThreadPoolUnitTests.cs
+++ src/IX.UnitTests/WorkOnThreadPoolUnitTests.cs
@@ -10,31 +10,36 @@ using Xunit;
 namespace IX.UnitTests
 {
     /// <summary>
-    /// Unit tests for working on thread pool.
+    ///     Unit tests for working on thread pool.
     /// </summary>
     public class WorkOnThreadPoolUnitTests
     {
         /// <summary>
-        /// Fact testing method.
+        ///     Tests running on the thread pool and, because of a lack of a synchronization context, not returning to the same
+        ///     thread.
         /// </summary>
-        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
-        [Fact(DisplayName = "Test running on thread pool.")]
+        /// <returns>A <see cref="Task" /> representing the asynchronous unit test.</returns>
+        [Fact(DisplayName = "Test running on thread pool and not returning to thread context.")]
         public async Task Test1()
         {
             // ARRANGE
-            int currentThreadId = Thread.CurrentThread.ManagedThreadId;
-            int separateThreadId = currentThreadId;
+            var currentThreadId = Thread.CurrentThread.ManagedThreadId;
+            var separateThreadId = currentThreadId;
+            int returningThreadId;
 
-            void LocalMethod()
-            {
-                separateThreadId = Thread.CurrentThread.ManagedThreadId;
-            }
+            void LocalMethod() { separateThreadId = Thread.CurrentThread.ManagedThreadId; }
 
             // ACT
             await Work.OnThreadPool(LocalMethod);
+            returningThreadId = Thread.CurrentThread.ManagedThreadId;
 
             // ASSERT
-            Assert.NotEqual(currentThreadId, separateThreadId);
+            Assert.NotEqual(
+                currentThreadId,
+                separateThreadId);
+            Assert.NotEqual(
+                currentThreadId,
+                returningThreadId);
         }
     }
 }
