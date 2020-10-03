0b23f7de6a8e5dc7bb23c9e2ec02a9fabf630f12
Adrian Mos <adimosh@outlook.com>
2020-10-03 22:52:53 +0300

    Async event and invoker.

---
+++ src/IX.StandardExtensions/EventModel/AsyncEventHandler.cs
@@ -0,0 +1,34 @@
+// <copyright file="AsyncEventHandler.cs" company="Adrian Mos">
+// Copyright (c) Adrian Mos with all rights reserved. Part of the IX Framework.
+// </copyright>
+
+using System;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace IX.StandardExtensions.EventModel
+{
+    /// <summary>
+    /// An asynchronous event handler delegate.
+    /// </summary>
+    /// <param name="sender">The sender.</param>
+    /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+    /// <returns>A task that can be awaited on by the event trigger.</returns>
+    [PublicAPI]
+    public delegate Task AsyncEventHandler(
+        object sender,
+        EventArgs e);
+
+    /// <summary>
+    /// An asynchronous event handler delegate.
+    /// </summary>
+    /// <typeparam name="TEventArguments">The type of the event arguments.</typeparam>
+    /// <param name="sender">The sender.</param>
+    /// <param name="e">The e.</param>
+    /// <returns>A task that can be awaited on by the event trigger.</returns>
+    [PublicAPI]
+    public delegate Task AsyncEventHandler<in TEventArguments>(
+        object sender,
+        TEventArguments e)
+        where TEventArguments : EventArgs;
+}
