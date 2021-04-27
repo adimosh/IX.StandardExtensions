c2367188a8419c29e3852aa6f94f607f7c7bcb58
Adrian Mos <adimosh@outlook.com>
2021-04-27 10:43:21 +0300

    Ordinal string extensions

--- src/IX.StandardExtensions/StandardExtensions/Globalization/StringExtensions.Ordinal.cs
+++ src/IX.StandardExtensions/StandardExtensions/Globalization/StringExtensions.Ordinal.cs
@@ -1,8 +1,9 @@
-// <copyright file="StringExtensions.InvariantCulture.cs" company="Adrian Mos">
+// <copyright file="StringExtensions.CurrentCulture.cs" company="Adrian Mos">
 // Copyright (c) Adrian Mos with all rights reserved. Part of the IX Framework.
 // </copyright>
 
-using System.Globalization;
+using System;
+using IX.StandardExtensions.Contracts;
 
 namespace IX.StandardExtensions.Globalization
 {
@@ -16,42 +17,42 @@ namespace IX.StandardExtensions.Globalization
 #region Static methods
 
         /// <summary>
-        ///     Compares the source string with a selected value in a case-sensitive manner using the comparison rules of the
-        ///     invariant culture.
+        ///     Compares the source string with a selected value in a case-sensitive manner using the comparison rules of the UI
+        ///     thread culture.
         /// </summary>
         /// <param name="source">The source to search in.</param>
         /// <param name="value">The string value to do the evaluation.</param>
         /// <returns>
         ///     The comparison of the two strings, with 0 meaning equality.
         /// </returns>
-        public static int InvariantCultureCompareTo(
+        public static int OrdinalCompareTo(
             this string source,
             string value) =>
-            CultureInfo.InvariantCulture.CompareInfo.Compare(
+            string.Compare(
                 source,
                 value,
-                CompareOptions.None);
+                StringComparison.Ordinal);
 
         /// <summary>
-        ///     Compares the source string with a selected value in a case-insensitive manner using the comparison rules of the
-        ///     invariant culture.
+        ///     Compares the source string with a selected value in a case-insensitive manner using the comparison rules of the UI
+        ///     thread culture.
         /// </summary>
         /// <param name="source">The source to search in.</param>
         /// <param name="value">The string value to do the evaluation.</param>
         /// <returns>
         ///     The comparison of the two strings, with 0 meaning equality.
         /// </returns>
-        public static int InvariantCultureCompareToInsensitive(
+        public static int OrdinalCompareToInsensitive(
             this string source,
             string value) =>
-            CultureInfo.InvariantCulture.CompareInfo.Compare(
+            string.Compare(
                 source,
                 value,
-                CompareOptions.IgnoreCase);
+                StringComparison.OrdinalIgnoreCase);
 
         /// <summary>
         ///     Determines whether a source string contains the specified value string in a case-sensitive manner using the
-        ///     comparison rules of the invariant culture.
+        ///     comparison rules of the UI thread culture.
         /// </summary>
         /// <param name="source">The source to search in.</param>
         /// <param name="value">The string value to do the evaluation.</param>
@@ -59,14 +60,14 @@ namespace IX.StandardExtensions.Globalization
         ///     <see langword="true" /> if the source string contains the specified value string; otherwise,
         ///     <see langword="false" />.
         /// </returns>
-        public static bool InvariantCultureContains(
+        public static bool OrdinalContains(
             this string source,
             string value) =>
-            source.InvariantCultureIndexOf(value) >= 0;
+            source.OrdinalIndexOf(value) >= 0;
 
         /// <summary>
         ///     Determines whether a source string contains the specified value string in a case-insensitive manner using the
-        ///     comparison rules of the invariant culture.
+        ///     comparison rules of the UI thread culture.
         /// </summary>
         /// <param name="source">The source to search in.</param>
         /// <param name="value">The string value to do the evaluation.</param>
@@ -74,218 +75,238 @@ namespace IX.StandardExtensions.Globalization
         ///     <see langword="true" /> if the source string contains the specified value string; otherwise,
         ///     <see langword="false" />.
         /// </returns>
-        public static bool InvariantCultureContainsInsensitive(
+        public static bool OrdinalContainsInsensitive(
             this string source,
             string value) =>
-            source.InvariantCultureIndexOfInsensitive(value) >= 0;
+            source.OrdinalIndexOfInsensitive(value) >= 0;
 
         /// <summary>
         ///     Checks whether or not the source string ends with a selected value in a case-sensitive manner using the comparison
-        ///     rules of the invariant culture.
+        ///     rules of the UI thread culture.
         /// </summary>
         /// <param name="source">The source to search in.</param>
         /// <param name="value">The string value to do the evaluation.</param>
         /// <returns>
         ///     <see langword="true" /> if the source string is equal to the value; otherwise, <see langword="false" />.
         /// </returns>
-        public static bool InvariantCultureEndsWith(
+        public static bool OrdinalEndsWith(
             this string source,
             string value) =>
-            CultureInfo.InvariantCulture.CompareInfo.IsSuffix(
-                source,
-                value,
-                CompareOptions.None);
+            Requires.NotNull(
+                    source,
+                    nameof(source))
+                .EndsWith(
+                    value,
+                    StringComparison.Ordinal);
 
         /// <summary>
         ///     Checks whether or not the source string ends with a selected value in a case-insensitive manner using the
-        ///     comparison rules of the invariant culture.
+        ///     comparison rules of the UI thread culture.
         /// </summary>
         /// <param name="source">The source to search in.</param>
         /// <param name="value">The string value to do the evaluation.</param>
         /// <returns>
         ///     <see langword="true" /> if the source string is equal to the value; otherwise, <see langword="false" />.
         /// </returns>
-        public static bool InvariantCultureEndsWithInsensitive(
+        public static bool OrdinalEndsWithInsensitive(
             this string source,
             string value) =>
-            CultureInfo.InvariantCulture.CompareInfo.IsSuffix(
-                source,
-                value,
-                CompareOptions.IgnoreCase);
+            Requires.NotNull(
+                    source,
+                    nameof(source))
+                .EndsWith(
+                    value,
+                    StringComparison.OrdinalIgnoreCase);
 
         /// <summary>
-        ///     Equates the source string with a selected value in a case-sensitive manner using the comparison rules of the
-        ///     invariant culture.
+        ///     Equates the source string with a selected value in a case-sensitive manner using the comparison rules of the UI
+        ///     thread culture.
         /// </summary>
         /// <param name="source">The source to search in.</param>
         /// <param name="value">The string value to do the evaluation.</param>
         /// <returns>
         ///     <see langword="true" /> if the source string is equal to the value; otherwise, <see langword="false" />.
         /// </returns>
-        public static bool InvariantCultureEquals(
+        public static bool OrdinalEquals(
             this string source,
             string value) =>
-            source.InvariantCultureCompareTo(value) == 0;
+            source.OrdinalCompareTo(value) == 0;
 
         /// <summary>
-        ///     Equates the source string with a selected value in a case-insensitive manner using the comparison rules of the
-        ///     invariant culture.
+        ///     Equates the source string with a selected value in a case-insensitive manner using the comparison rules of the UI
+        ///     thread culture.
         /// </summary>
         /// <param name="source">The source to search in.</param>
         /// <param name="value">The string value to do the evaluation.</param>
         /// <returns>
         ///     <see langword="true" /> if the source string is equal to the value; otherwise, <see langword="false" />.
         /// </returns>
-        public static bool InvariantCultureEqualsInsensitive(
+        public static bool OrdinalEqualsInsensitive(
             this string source,
             string value) =>
-            source.InvariantCultureCompareToInsensitive(value) == 0;
+            source.OrdinalCompareToInsensitive(value) == 0;
 
         /// <summary>
         ///     Finds the index of the specified value string in a source string in a case-sensitive manner using the comparison
-        ///     rules of the invariant culture.
+        ///     rules of the UI thread culture.
         /// </summary>
         /// <param name="source">The source to search in.</param>
         /// <param name="value">The string value to do the evaluation.</param>
         /// <returns>
         ///     The index where the string is found, otherwise -1.
         /// </returns>
-        public static int InvariantCultureIndexOf(
+        public static int OrdinalIndexOf(
             this string source,
             string value) =>
-            CultureInfo.InvariantCulture.CompareInfo.IndexOf(
-                source,
-                value,
-                CompareOptions.None);
+            Requires.NotNull(
+                    source,
+                    nameof(source))
+                .IndexOf(
+                    value,
+                    StringComparison.Ordinal);
 
         /// <summary>
         ///     Finds the index of the specified value string in a source string in a case-sensitive manner using the comparison
-        ///     rules of the invariant culture.
+        ///     rules of the UI thread culture.
         /// </summary>
         /// <param name="source">The source to search in.</param>
         /// <param name="value">The string value to do the evaluation.</param>
         /// <param name="startIndex">The index in the source string to start searching at.</param>
         /// <returns>The index where the string is found, otherwise -1.</returns>
-        public static int InvariantCultureIndexOf(
+        public static int OrdinalIndexOf(
             this string source,
             string value,
             int startIndex) =>
-            CultureInfo.InvariantCulture.CompareInfo.IndexOf(
-                source,
-                value,
-                startIndex,
-                CompareOptions.None);
+            Requires.NotNull(
+                    source,
+                    nameof(source))
+                .IndexOf(
+                    value,
+                    startIndex,
+                    StringComparison.Ordinal);
 
         /// <summary>
         ///     Finds the index of the specified value string in a source string in a case-sensitive manner using the comparison
-        ///     rules of the invariant culture.
+        ///     rules of the UI thread culture.
         /// </summary>
         /// <param name="source">The source to search in.</param>
         /// <param name="value">The string value to do the evaluation.</param>
         /// <param name="startIndex">The index in the source string to start searching at.</param>
         /// <param name="count">The number of characters to search.</param>
         /// <returns>The index where the string is found, otherwise -1.</returns>
-        public static int InvariantCultureIndexOf(
+        public static int OrdinalIndexOf(
             this string source,
             string value,
             int startIndex,
             int count) =>
-            CultureInfo.InvariantCulture.CompareInfo.IndexOf(
-                source,
-                value,
-                startIndex,
-                count,
-                CompareOptions.None);
+            Requires.NotNull(
+                    source,
+                    nameof(source))
+                .IndexOf(
+                    value,
+                    startIndex,
+                    count,
+                    StringComparison.Ordinal);
 
         /// <summary>
         ///     Finds the index of the specified value string in a source string in a case-insensitive manner using the comparison
-        ///     rules of the invariant culture.
+        ///     rules of the UI thread culture.
         /// </summary>
         /// <param name="source">The source to search in.</param>
         /// <param name="value">The string value to do the evaluation.</param>
         /// <returns>
         ///     The index where the string is found, otherwise -1.
         /// </returns>
-        public static int InvariantCultureIndexOfInsensitive(
+        public static int OrdinalIndexOfInsensitive(
             this string source,
             string value) =>
-            CultureInfo.InvariantCulture.CompareInfo.IndexOf(
-                source,
-                value,
-                CompareOptions.IgnoreCase);
+            Requires.NotNull(
+                    source,
+                    nameof(source))
+                .IndexOf(
+                    value,
+                    StringComparison.OrdinalIgnoreCase);
 
         /// <summary>
         ///     Finds the index of the specified value string in a source string in a case-insensitive manner using the comparison
-        ///     rules of the invariant culture.
+        ///     rules of the UI thread culture.
         /// </summary>
         /// <param name="source">The source to search in.</param>
         /// <param name="value">The string value to do the evaluation.</param>
         /// <param name="startIndex">The index in the source string to start searching at.</param>
         /// <returns>The index where the string is found, otherwise -1.</returns>
-        public static int InvariantCultureIndexOfInsensitive(
+        public static int OrdinalIndexOfInsensitive(
             this string source,
             string value,
             int startIndex) =>
-            CultureInfo.InvariantCulture.CompareInfo.IndexOf(
-                source,
-                value,
-                startIndex,
-                CompareOptions.IgnoreCase);
+            Requires.NotNull(
+                    source,
+                    nameof(source))
+                .IndexOf(
+                    value,
+                    startIndex,
+                    StringComparison.OrdinalIgnoreCase);
 
         /// <summary>
         ///     Finds the index of the specified value string in a source string in a case-insensitive manner using the comparison
-        ///     rules of the invariant culture.
+        ///     rules of the UI thread culture.
         /// </summary>
         /// <param name="source">The source to search in.</param>
         /// <param name="value">The string value to do the evaluation.</param>
         /// <param name="startIndex">The index in the source string to start searching at.</param>
         /// <param name="count">The number of characters to search.</param>
         /// <returns>The index where the string is found, otherwise -1.</returns>
-        public static int InvariantCultureIndexOfInsensitive(
+        public static int OrdinalIndexOfInsensitive(
             this string source,
             string value,
             int startIndex,
             int count) =>
-            CultureInfo.InvariantCulture.CompareInfo.IndexOf(
-                source,
-                value,
-                startIndex,
-                count,
-                CompareOptions.IgnoreCase);
+            Requires.NotNull(
+                    source,
+                    nameof(source))
+                .IndexOf(
+                    value,
+                    startIndex,
+                    count,
+                    StringComparison.OrdinalIgnoreCase);
 
         /// <summary>
         ///     Checks whether or not the source string starts with a selected value in a case-sensitive manner using the
-        ///     comparison rules of the invariant culture.
+        ///     comparison rules of the UI thread culture.
         /// </summary>
         /// <param name="source">The source to search in.</param>
         /// <param name="value">The string value to do the evaluation.</param>
         /// <returns>
         ///     <see langword="true" /> if the source string is equal to the value; otherwise, <see langword="false" />.
         /// </returns>
-        public static bool InvariantCultureStartsWith(
+        public static bool OrdinalStartsWith(
             this string source,
             string value) =>
-            CultureInfo.InvariantCulture.CompareInfo.IsPrefix(
-                source,
-                value,
-                CompareOptions.None);
+            Requires.NotNull(
+                    source,
+                    nameof(source))
+                .StartsWith(
+                    value,
+                    StringComparison.Ordinal);
 
         /// <summary>
         ///     Checks whether or not the source string starts with a selected value in a case-insensitive manner using the
-        ///     comparison rules of the invariant culture.
+        ///     comparison rules of the UI thread culture.
         /// </summary>
         /// <param name="source">The source to search in.</param>
         /// <param name="value">The string value to do the evaluation.</param>
         /// <returns>
         ///     <see langword="true" /> if the source string is equal to the value; otherwise, <see langword="false" />.
         /// </returns>
-        public static bool InvariantCultureStartsWithInsensitive(
+        public static bool OrdinalStartsWithInsensitive(
             this string source,
             string value) =>
-            CultureInfo.InvariantCulture.CompareInfo.IsPrefix(
-                source,
-                value,
-                CompareOptions.IgnoreCase);
+            Requires.NotNull(
+                    source,
+                    nameof(source))
+                .StartsWith(
+                    value,
+                    StringComparison.OrdinalIgnoreCase);
 
 #endregion
 
