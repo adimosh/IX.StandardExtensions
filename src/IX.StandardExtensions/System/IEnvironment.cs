225d28d7084e39653ecfbf46438c08ad17f97863
Adrian Mos <adimosh@outlook.com>
2021-05-18 09:08:45 +0300

    Environment abstractions.

---
+++ src/IX.StandardExtensions/System/IEnvironment.cs
@@ -0,0 +1,15 @@
+using GlobalSystem = System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IX.System
+{
+    /// <summary>
+    /// Represents an abstraction over the <see cref="GlobalSystem.Environment"/> class.
+    /// </summary>
+    public interface IEnvironment
+    {
+    }
+}
