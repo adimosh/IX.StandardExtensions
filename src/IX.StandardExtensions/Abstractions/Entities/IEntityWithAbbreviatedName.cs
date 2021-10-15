c66ec41d38cf1b4243f7a297a5d633dcdee66bed
Adrian Mos <adimosh@outlook.com>
2021-10-15 19:23:34 +0300

    New entity abstractions

---
+++ src/IX.StandardExtensions/Abstractions/Entities/IEntityWithAbbreviatedName.cs
@@ -0,0 +1,27 @@
+// <copyright file="IEntityWithAbbreviatedName.cs" company="Adrian Mos">
+// Copyright (c) Adrian Mos with all rights reserved. Part of the IX Framework.
+// </copyright>
+
+using JetBrains.Annotations;
+
+namespace IX.Abstractions.Entities
+{
+    /// <summary>
+    ///     A data contract for an entity with an abbreviated name.
+    /// </summary>
+    [PublicAPI]
+    public interface IEntityWithAbbreviatedName
+    {
+#region Properties and indexers
+
+        /// <summary>
+        ///     Gets or sets the abbreviation for this entity.
+        /// </summary>
+        /// <value>
+        ///     The entity abbreviation.
+        /// </value>
+        public string Abbreviation { get; set; }
+
+#endregion
+    }
+}
---
+++ src/IX.StandardExtensions/Abstractions/Entities/INamedEntity.cs
@@ -0,0 +1,27 @@
+// <copyright file="INamedEntity.cs" company="Adrian Mos">
+// Copyright (c) Adrian Mos with all rights reserved. Part of the IX Framework.
+// </copyright>
+
+using JetBrains.Annotations;
+
+namespace IX.Abstractions.Entities
+{
+    /// <summary>
+    ///     A data contract for a named entity.
+    /// </summary>
+    [PublicAPI]
+    public interface INamedEntity
+    {
+#region Properties and indexers
+
+        /// <summary>
+        ///     Gets or sets a name for this entity.
+        /// </summary>
+        /// <value>
+        ///     The entity name.
+        /// </value>
+        public string Name { get; set; }
+
+#endregion
+    }
+}
